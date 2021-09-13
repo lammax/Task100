03fbb7f36d4731cb100cb189489df8a22764269b
Maksim.Lamanskiy <lammax2012@gmail.com>
2021-09-13 22:48:51 +0300

    Initial Commit

---
+++ Task100/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Task100
+//
+//  Created by Максим Ламанский on 13.09.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Task100/Task100App.swift
@@ -0,0 +1,17 @@
+//
+//  Task100App.swift
+//  Task100
+//
+//  Created by Максим Ламанский on 13.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct Task100App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
